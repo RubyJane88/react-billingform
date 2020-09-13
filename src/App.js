1f891c5f596f2c527a1c6afe8486a190fa943ea8
RJ Cabagnot <rjcabagnot@hotmail.com>
2020-09-13 20:55:04 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import FormikBillingform from "./components/formik-billingform";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-
 import { initialValues } from "./components/forms-schema/formik-initialvalues";
 import { schema } from "./components/forms-schema/formik-yupvalidation";
 import FormDivider from "./components/forms-schema/form-divider";
+import CustomHeaderBar from "./components/forms-schema/CustomHeaderBar";
+import TitlePage from "./components/forms-schema/title-page";
 
 function App() {
   const classes = useStyles();
@@ -20,24 +16,16 @@ function App() {
   return (
     <div>
       <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            />
-            <Typography variant="h6" className={classes.title}>
-              Dashboard
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
+        <CustomHeaderBar classes={classes} />
       </div>
+
       <Container>
-        <h1> Billing Information</h1>
-        <p> The following information will be displayed on your invoices</p>
+        <TitlePage
+          title={"Billing Information"}
+          description={
+            "The following information will be displayed on your invoices"
+          }
+        />
         <FormDivider />
         <FormikBillingform
           initialValues={initialValues}
---
+++ src/components/forms-schema/CustomHeaderBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const CustomHeaderBar = ({ classes }) => (
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+      />
+      <Typography variant="h6" className={classes.title}>
+        Dashboard
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Toolbar>
+  </AppBar>
+);
+
+export default CustomHeaderBar;
